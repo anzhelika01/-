159cef2d50fa629c6f943f634536678a75bec743
Anzhey Sluka <sluka02@mail.ru>
2020-05-25 16:50:25 +0300

    Добавьте файлы проекта.

---
+++ Евклид/Form1.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Форма переопределяет dispose для очистки списка компонентов.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Является обязательной для конструктора форм Windows Forms
+	Private components As System.ComponentModel.IContainer
+
+	'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+	'Для ее изменения используйте конструктор форм Windows Form.  
+	'Не изменяйте ее в редакторе исходного кода.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.Button2 = New System.Windows.Forms.Button()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.TextBox3 = New System.Windows.Forms.TextBox()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.SuspendLayout()
+		'
+		'Button2
+		'
+		Me.Button2.BackColor = System.Drawing.Color.Aqua
+		Me.Button2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+		Me.Button2.ForeColor = System.Drawing.Color.Black
+		Me.Button2.Location = New System.Drawing.Point(149, 183)
+		Me.Button2.Name = "Button2"
+		Me.Button2.Size = New System.Drawing.Size(108, 34)
+		Me.Button2.TabIndex = 8
+		Me.Button2.Text = "Очистить"
+		Me.Button2.UseVisualStyleBackColor = False
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+		Me.Label3.ForeColor = System.Drawing.Color.Black
+		Me.Label3.Location = New System.Drawing.Point(51, 116)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(18, 19)
+		Me.Label3.TabIndex = 7
+		Me.Label3.Text = "c"
+		'
+		'TextBox3
+		'
+		Me.TextBox3.Location = New System.Drawing.Point(100, 116)
+		Me.TextBox3.Name = "TextBox3"
+		Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+		Me.TextBox3.TabIndex = 4
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.Black
+		Me.Label1.Location = New System.Drawing.Point(50, 40)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(18, 19)
+		Me.Label1.TabIndex = 2
+		Me.Label1.Text = "a"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+		Me.Label2.ForeColor = System.Drawing.Color.Black
+		Me.Label2.Location = New System.Drawing.Point(50, 78)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(19, 19)
+		Me.Label2.TabIndex = 6
+		Me.Label2.Text = "b"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(100, 40)
+		Me.TextBox1.Multiline = True
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+		Me.TextBox1.TabIndex = 1
+		'
+		'Button1
+		'
+		Me.Button1.BackColor = System.Drawing.Color.Aqua
+		Me.Button1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+		Me.Button1.ForeColor = System.Drawing.Color.Black
+		Me.Button1.Location = New System.Drawing.Point(12, 183)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(111, 34)
+		Me.Button1.TabIndex = 0
+		Me.Button1.Text = "Посчитать"
+		Me.Button1.UseVisualStyleBackColor = False
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(100, 79)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+		Me.TextBox2.TabIndex = 3
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+		Me.ClientSize = New System.Drawing.Size(273, 245)
+		Me.Controls.Add(Me.Button2)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.TextBox3)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Name = "Form1"
+		Me.Text = "Евклид"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Button2 As Button
+	Friend WithEvents Label3 As Label
+	Friend WithEvents TextBox3 As TextBox
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents Button1 As Button
+	Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Евклид/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim a As Integer
+    Dim b As Integer
+    Dim c As Integer
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+    Private Sub Algoritm()
+        Do While a <> 0 And b <> 0
+            If a > b Then
+                a = a Mod b
+            Else
+                b = b Mod a
+            End If
+        Loop
+        c = a + b
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Try
+                a = Integer.Parse(TextBox1.Text)
+            Catch ex As Exception
+                MsgBox("Enter an integer")
+                Throw ex
+            End Try
+            Try
+                b = Integer.Parse(TextBox2.Text)
+            Catch ex As Exception
+                MsgBox("Enter an integer")
+                Throw ex
+            End Try
+        Catch ex As Exception
+            Return
+        End Try
+        Algoritm()
+        TextBox3.Text = c.ToString
+    End Sub
+End Class
